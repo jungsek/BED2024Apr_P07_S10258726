e5e208ec6ea33294c6337cdd7c39c9b876bbfae2
jungsek <s10258726@connect.np.edu.sg>
2024-05-02 10:00:39 +0800

    Last commit

--- Week03/books-api-mvc/app.js
+++ Week03/books-api-mvc/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const booksController = require("./controllers/booksController");
-const validateBook = require("./middlewares/validateBook");
+const validateBook = require('./middlewares/validatebook');
 const app = express();
 
 app.use(bodyParser.json()); //Parse the incoming json data in request body
@@ -9,15 +9,14 @@ app.use(bodyParser.urlencoded({extended:true})); //For form data handling
 
 app.get("/books", booksController.getAllBooks);
 app.get("/books/:id", booksController.getBookById);
-app.post("/books", booksController.createBook);
-app.put("/books", booksController.updateBook);
-app.delete("/books", booksController.deleteBook);
 app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
 app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
+app.delete("/books", booksController.deleteBook);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
 
