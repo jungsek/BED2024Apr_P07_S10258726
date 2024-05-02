1bc355ab57a504fbada7ad0ccbb9ea363bc477bb
jungsek <s10258726@connect.np.edu.sg>
2024-05-02 09:55:24 +0800

    validatebook

--- Week03/books-api-mvc/app.js
+++ Week03/books-api-mvc/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const booksController = require("./controllers/booksController");
+const validateBook = require("./middlewares/validateBook");
 const app = express();
 
 app.use(bodyParser.json()); //Parse the incoming json data in request body
@@ -11,12 +12,12 @@ app.get("/books/:id", booksController.getBookById);
 app.post("/books", booksController.createBook);
 app.put("/books", booksController.updateBook);
 app.delete("/books", booksController.deleteBook);
+app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
+app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
 
